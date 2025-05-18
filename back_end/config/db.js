153b3844b95f4cafbb23937029cfbce945c86d02
Arjun Rao <125185224+arjun1127@users.noreply.github.com>
2025-05-18 08:18:28 +0530

    Update db.js

--- back_end/config/db.js
+++ back_end/config/db.js
@@ -9,4 +9,3 @@ export const sql=neon(
     `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 )
 //sql fun is used as a tagged literal ,which allws to write sql quires
-//postgresql://neondb_owner:npg_q9Aj8HigOPCz@ep-jolly-star-a80iycqx-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
